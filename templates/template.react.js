9d8ab250e47803744e0e352dac78ee07414eb390
Andrew <gandriy123wf@gmail.com>
2020-07-19 00:31:00 +0200

    add metadata to template

--- templates/template.react.js
+++ templates/template.react.js
@@ -4,7 +4,8 @@ function template(markup, css) {
       <html lang="en">
         <head>
            <meta charset="utf-8">
-           <title>Project One</title>
+           <meta name="description" content="Hi! This is the platform for popularization science, social and media content of ukrainian authors and global conjuncture without political point">
+           <title>Karambol - Platform with media, social and scientific content</title>
            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
            <script src="https://apis.google.com/js/platform.js?onload=init" async defer></script>  
