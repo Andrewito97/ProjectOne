9ab540057bbf83958855c36aa3e4fab4998d1af0
AndrewLebowski <gandriy123wf@gmail.com>
2021-01-28 23:49:55 +0200

    change link on icon

--- templates/template.react.js
+++ templates/template.react.js
@@ -11,7 +11,7 @@ function template(markup, css, deviceCheck, trackingId) {
            The main goal - to share basic knowledge which everyone should know in modern world.
            We hope our platform will help you understand all aspects that interest you and become better.">
            <title>Karambol - fundamental knowledge</title>
-           <link rel="shortcut icon" href="../client/assets/ukraine.png" type="image/png">
+           <link rel="shortcut icon" href="../client/assets/carambola.png" type="image/png">
            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />  
            <script type="text/plain" cookie-consent="targeting" src="https://apis.google.com/js/platform.js?onload=init" async defer></script>
